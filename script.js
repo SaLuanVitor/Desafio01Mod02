1251e6bb84aabf24aac2bf35d148fef5d9056daa
Luan Vitor <luanvitors_852@hotmail.com>
2022-06-09 12:28:53 -0300

    Primeiro desafio em JavaScript pelo VaiNaWeb

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// Desafio 01 VaiNaWeb Mod02
+
+let nome = "Luan";
+let idade = 22;
+
+let anoAtual = 2022;
+let result = anoAtual - idade;
+
+console.log(anoAtual + result);
+console.log("Estou mexendo no console.log");
+console.log("----------------------------------------");
+
+let quartaFeira = false;
+console.log("Hoje é quarta-feira?", quartaFeira);
+console.log("quartaFeira é um dado do tipo:", typeof quartaFeira);
